24fda2a20e5d7f44d039ae1be517d22173f574fa
Tahoun99 <103882114+Tahoun99@users.noreply.github.com>
2024-07-22 13:56:57 +0300

    Update ReadingLKP.cs

--- Models/ReadingLKP.cs
+++ Models/ReadingLKP.cs
@@ -1,14 +1,13 @@
 ﻿using App.Models;
 using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class ReadingLKP
+namespace App.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }= String.Empty;
-    public string Unit { get; set; } = String.Empty;
-    public virtual ICollection<ActualReadings> ActualReadings { get; set; }
-
+    public class ReadingLKP
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Unit { get; set; } = String.Empty;
+        public virtual ICollection<ActualReadings> ActualReadings { get; set; }
+    }
 }
