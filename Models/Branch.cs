5105a7ba710307ceb44fb74f21a455cb490b1179
Tahoun99 <103882114+Tahoun99@users.noreply.github.com>
2024-07-22 13:16:22 +0300

    one to many example

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
-
-public class Branch
+namespace MQTTReader.Models
 {
-    public int Id { get; set; }
-    public string String { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public string PostalCode { get; set; }
+
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string String { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PostalCode { get; set; }
+    }
 }
