9c44adfd64811f8a2a5c0234f4a2c221d6e5f38a
mario3fady <161719856+mario3fady@users.noreply.github.com>
2024-07-22 13:38:20 +0300

    ActualReadingsDone

--- Models/ActualReading.cs
+++ Models/ActualReading.cs
@@ -1,13 +1,19 @@
 using System;
+using App.Models;
+namespace App.Models {
 
-public class ActualReadings
-{
-    public int Id { get; set; }
-    public int DeviceId { get; set; }
-    public int ReadingLKPId { get; set; }
-    public decimal ReadingValue { get; set; }
-    public DateTime TimeStamp { get; set; } = DateTime.Now;
+    public class ActualReadings
+    {
+        public int Id { get; set; }
+        public int DeviceId { get; set; }
+        public int ReadingLKPId { get; set; }
+        public decimal ReadingValue { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+        public Device device { get; set; }
+        public ReadingLKP readingLKP { get; set; }
 
 
-    
+
+    }
 }
