076b5df070ae0fa6cd1fa1e34cb5b49691d85faf
LORDyyyyy <mlord6271@gmail.com>
2024-07-22 13:17:51 +0300

    backgroud services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using App.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<DataProcessingService>();
 
 var app = builder.Build();
 
