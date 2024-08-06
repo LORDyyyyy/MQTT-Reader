e3c04743a7be5c80ee84ed4fe89954d445d162b4
Tahoun99 <103882114+Tahoun99@users.noreply.github.com>
2024-08-06 21:09:31 +0300

    Create BranchDTO

---
+++ DTO/BranchDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace App.DTO
+{
+    public class BranchDTO
+    {
+        public int Id { get; set; }
+        public string String { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PostalCode { get; set; } = String.Empty;
+    }
+}
